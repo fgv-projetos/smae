81798ccfb454a492813ba693fc08cf0f297e0650
Lucas Ansei <takushilucas@gmail.com>
2026-03-31 16:47:15 -0300

    feat(workflow): adicionando suporte a busca com vetores e paginação

--- backend/src/casa-civil/workflow/configuracao/dto/filter-workflow.dto.ts
+++ backend/src/casa-civil/workflow/configuracao/dto/filter-workflow.dto.ts
@@ -1,5 +1,5 @@
-import { Transform } from 'class-transformer';
-import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsBoolean, IsInt, IsNumber, IsOptional, IsString, Max, MaxLength, Min } from 'class-validator';
 
 export class FilterWorkflowDto {
     @IsOptional()
@@ -10,6 +10,40 @@ export class FilterWorkflowDto {
     @IsOptional()
     @IsNumber()
     transferencia_tipo_id?: number;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(250)
+    palavra_chave?: string;
+
+    /**
+     * Itens por página, padrão 25
+     * @example "25"
+     */
+    @IsOptional()
+    @IsInt()
+    @Max(500)
+    @Min(1)
+    @Transform((a: TransformFnParams) => (a.value === '' ? undefined : +a.value))
+    ipp?: number;
+
+    /**
+     * Número da página, padrão 1
+     * @example "1"
+     */
+    @IsOptional()
+    @IsInt()
+    @Min(1)
+    @Transform((a: TransformFnParams) => (a.value === '' ? undefined : +a.value))
+    pagina?: number;
+
+    /**
+     * Token de paginação
+     */
+    @IsOptional()
+    @IsString()
+    @MaxLength(2000)
+    token_paginacao?: string;
 }
 
 export class FilterWorkflowEtapaDto {

714fb608ffe41caf81a8334272e7c30e122f7d98
Lucas Ansei <takushilucas@gmail.com>
2026-04-01 12:21:58 -0300

    fix: migration

--- backend/prisma/migrations/20260401142107_demanda_email_parlamentar/migration.sql
+++ backend/prisma/migrations/20260401142107_demanda_email_parlamentar/migration.sql
@@ -6,16 +6,14 @@
 
 */
 -- AlterTable
-ALTER TABLE "demanda_email_parlamentar"
-    ALTER COLUMN "assunto" SET DEFAULT '',
-    ALTER COLUMN "corpo" SET DEFAULT '';
+ALTER TABLE "demanda_email_parlamentar" ALTER COLUMN "assunto" SET DEFAULT '';
+ALTER TABLE "demanda_email_parlamentar" ALTER COLUMN "corpo" SET DEFAULT '';
 
 UPDATE "demanda_email_parlamentar" SET "assunto" = '' WHERE "assunto" IS NULL;
 UPDATE "demanda_email_parlamentar" SET "corpo" = '' WHERE "corpo" IS NULL;
 
-ALTER TABLE "demanda_email_parlamentar"
-    ALTER COLUMN "assunto" SET NOT NULL,
-    ALTER COLUMN "corpo" SET NOT NULL;
+ALTER TABLE "demanda_email_parlamentar" ALTER COLUMN "assunto" SET NOT NULL;
+ALTER TABLE "demanda_email_parlamentar" ALTER COLUMN "corpo" SET NOT NULL;
 
 -- AlterTable
 ALTER TABLE "workflow" ALTER COLUMN "vetores_busca" DROP DEFAULT;
